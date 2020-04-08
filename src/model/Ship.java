f670b2c0afa2e619fae4a05aff0d29b2c2786273
MitoCode <61301128+paoos9513@users.noreply.github.com>
2020-04-08 14:48:04 -0500

    codigo terminado, hace falta comentarios y contratos

--- src/model/Ship.java
+++ src/model/Ship.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.ArrayList;
+
 public class Ship {
 
     private final static double MAXVALUE = 28000;
@@ -7,6 +9,8 @@ public class Ship {
     private String captain;
 
     private Client[] clients;
+
+    private ArrayList<Load> myLoads;
 
     private double totalWeight;
 
@@ -20,6 +24,174 @@ public class Ship {
 
     }
 
+    public String addLoad(Load newLoad) {
+
+        String msg = "";
+
+        boolean perecedera = false;
+        boolean peligrosa = false;
+
+        for (int i = 0; i < myLoads.size(); i++) {
+
+            if (myLoads.get(i).getTypeLoad().equals("PERECEDERA")) {
+                perecedera = true;
+            }
+
+            if (myLoads.get(i).getTypeLoad().equals("PELIGROSA")) {
+                peligrosa = true;
+            }
+
+        }
+
+        if (totalWeight < MAXVALUE) {
+
+            if (perecedera && newLoad.getTypeLoad().equals("PELIGROSA")) {
+
+                for (int i = 0; i < myLoads.size(); i++) {
+                    if (myLoads.get(i).getOwner().getName().equals(newLoad.getOwner().getName())) {
+                        myLoads.remove(i);
+                    }
+                }
+
+                msg = "No se pudo cargar por que no cumple las condiciones.";
+
+            } else {
+
+                msg = "Se puede cargar y su valor incluyendo un posible descuento es: $" + costOfLoad(newLoad);
+
+                myLoads.add(newLoad);
+
+                totalWeight += newLoad.getWeightByLoad() * newLoad.getNumBoxes();
+
+                for (int i = 0; i < clients.length; i++) {
+                    if (newLoad.getOwner().getName().equals(clients[i].getName())) {
+                        clients[i].setWeightAccum(
+                                clients[i].getWeightAccum() + (newLoad.getNumBoxes() * newLoad.getWeightByLoad()));
+                        clients[i].setMoneyAccum(clients[i].getMoneyAccum() + (costOfLoad(newLoad)));
+                    }
+                }
+
+            }
+
+            if (peligrosa && newLoad.getTypeLoad().equals("PERECEDERA")) {
+
+                for (int i = 0; i < myLoads.size(); i++) {
+                    if (myLoads.get(i).getOwner().getName().equals(newLoad.getOwner().getName())) {
+                        myLoads.remove(i);
+                    }
+                }
+
+                msg = "No se pudo cargar por que no cumple las condiciones.";
+
+            } else {
+
+                msg = "Se puede cargar y su valor incluyendo un posible descuento es: $" + costOfLoad(newLoad);
+
+                myLoads.add(newLoad);
+
+                totalWeight += newLoad.getWeightByLoad() * newLoad.getNumBoxes();
+
+                for (int i = 0; i < clients.length; i++) {
+                    if (newLoad.getOwner().getName().equals(clients[i].getName())) {
+                        clients[i].setWeightAccum(
+                                clients[i].getWeightAccum() + (newLoad.getNumBoxes() * newLoad.getWeightByLoad()));
+                        clients[i].setMoneyAccum(clients[i].getMoneyAccum() + (costOfLoad(newLoad)));
+                    }
+                }
+
+            }
+
+        } else {
+
+            for (int i = 0; i < myLoads.size(); i++) {
+                if (myLoads.get(i).getOwner().getName().equals(newLoad.getOwner().getName())) {
+                    myLoads.remove(i);
+                }
+            }
+
+            msg = "No se pudo cargar porque excede el peso permitido.";
+        }
+
+        return msg;
+
+    }
+
+    public String emptyShip() {
+
+        myLoads.clear();
+        totalWeight = 0;
+
+        return "Barco descargado";
+    }
+
+    public String canSail() {
+        String result = "No puede zarpar aun.";
+
+        System.out.println(myLoads.size());
+        if (myLoads.size() > 2) {
+            result = "Si se puede zarpar";
+        }
+
+        System.out.println(totalWeight);
+        if (totalWeight > 12000) {
+            result = "Si se puede zarpar";
+        }
+
+        return result;
+    }
+
+    public double costOfLoad(Load newLoad) {
+        double cost = 0;
+        double totalWeightOfBoxes = newLoad.getWeightByLoad() * newLoad.getNumBoxes();
+
+        if (newLoad.getTypeLoad().equals(Load.TYPELOAD[0])) {
+            cost = totalWeightOfBoxes * 390000;
+        } else if (newLoad.getTypeLoad().equals(Load.TYPELOAD[1])) {
+            cost = totalWeightOfBoxes * 250000;
+        } else {
+            cost = totalWeightOfBoxes * 80000;
+        }
+
+        if (newLoad.getOwner().getTypeClient().equals(Client.TYPESCLIENT[1])
+                && newLoad.getTypeLoad().equals(Load.TYPELOAD[1])) {
+            cost = cost - (cost * 0.015);
+        }
+
+        if (newLoad.getOwner().getTypeClient().equals(Client.TYPESCLIENT[2])
+                && (newLoad.getTypeLoad().equals(Load.TYPELOAD[1]) || newLoad.getTypeLoad().equals(Load.TYPELOAD[2]))) {
+            cost = cost - (cost * 0.03);
+        }
+
+        if (newLoad.getOwner().getTypeClient().equals(Client.TYPESCLIENT[3])) {
+            cost = cost - (cost * 0.05);
+        }
+
+        return cost;
+    }
+
+    public String upgradeClient(int numClient) {
+        String result = "";
+
+        if (clients[numClient - 1].getTypeClient().equals("Normal")) {
+            if (clients[numClient - 1].getWeightAccum() >= 35000) {
+                result = "Felicidades!!! Este cliente subio de categoria a Plata";
+            }
+        }
+
+        if (clients[numClient - 1].getTypeClient().equals("Plata")) {
+            if (clients[numClient - 1].getWeightAccum() >= 55000 || clients[numClient - 1].getMoneyAccum() >= 2000000) {
+                result = "Felicidades!!! Este cliente subio de categoria a Oro";
+            }
+        }
+
+        if (clients[numClient - 1].getTypeClient().equals("Oro")) {
+            if (clients[numClient - 1].getMoneyAccum() >= 5000000) {
+                result = "Felicidades!!! Este cliente subio de categoria a Platinum";
+            }
+        }
+
+        return result;
+    }
 
     public String getCaptain() {
         return captain;
@@ -35,6 +207,14 @@ public class Ship {
 
     public void setClients(Client[] clients) {
         this.clients = clients;
+    }
+
+    public ArrayList<Load> getMyLoads() {
+        return myLoads;
+    }
+
+    public void setMyLoads(ArrayList<Load> myLoads) {
+        this.myLoads = myLoads;
     }
 
     public double getTotalWeight() {
--- src/ui/Pirate.java
+++ src/ui/Pirate.java
@@ -13,5 +13,87 @@ public class Pirate {
         elPirata = new Ship();
         Scanner r = new Scanner(System.in);
 
+        int opt = -1;
+
+        System.out.println("Por favor agrege los 5 clientes: \n");
+        Client clients[] = new Client[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Ingrese nombre del cliente " + (i + 1));
+            String name = r.nextLine();
+            System.out.println("Ingrese el numero de registro mercantil " + (i + 1));
+            String commercialRNumber = r.nextLine();
+            System.out.println("Ingrese la fecha de registro (dd-mm-aaaa)");
+            String dateCR = r.nextLine();
+
+            clients[i] = new Client(name, commercialRNumber, dateCR);
+        }
+
+        elPirata.setClients(clients);
+
+        do {
+            System.out.println(
+                    "Escoja la opcion que desee:\n\n(1) Cargar el barco\n(2) Descargar el barco para iniciar un nuevo viaje\n(3) Mostrar peso total de la carga que transporta el barco\n(4) Verificar si se puede zarpar\n(5) Salir");
+
+            opt = r.nextInt();
+
+            switch (opt) {
+                case 1:
+                    System.out.println("Escoja el cliente:");
+
+                    for (int i = 0; i < elPirata.getClients().length; i++) {
+                        System.out.println((i + 1) + " " + elPirata.getClients()[i].getName());
+                    }
+
+                    int numClient = r.nextInt();
+                    Client owner = elPirata.getClients()[numClient - 1];
+
+                    System.out.println("Escoja el tipo de carga");
+                    for (int i = 0; i < Load.TYPELOAD.length; i++) {
+                        System.out.println((i + 1) + " " + Load.TYPELOAD[i]);
+                    }
+                    String typeLoad = Load.TYPELOAD[r.nextInt() - 1];
+
+                    System.out.println("Ingrese el peso de cada caja en gramos");
+                    double weightByLoad = (r.nextDouble() * 0.001);
+
+                    System.out.println("Ingrese el numero de cajas");
+                    int numBoxes = r.nextInt();
+
+                    Load newLoad = new Load(typeLoad, weightByLoad, numBoxes, owner);
+
+                    System.out.println(elPirata.addLoad(newLoad));
+
+                    System.out.println(elPirata.upgradeClient(numClient));
+
+                    break;
+
+                case 2:
+                    System.out.println(elPirata.emptyShip());
+                    break;
+
+                case 3:
+                    System.out.println(
+                            "El peso total de la carga hasta el momento es: " + elPirata.getTotalWeight() + "kilos");
+                    break;
+
+                case 4:
+                    System.out.println(elPirata.canSail());
+                    break;
+
+                case 5:
+                    System.out.println("Cerrando el programa");
+
+                    break;
+
+                default:
+                    System.out.println("Opcion invalida, vuelva a intentar");
+                    break;
+            }
+
+        } while (opt != 5);
+
+        r.close();
+
     }
 }
